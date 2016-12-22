a06575b09021761567a8b86e4d6587f1f643fccf
Kris Herlaar <kris@theredhead.nl>
2016-12-22 08:33:24 +0100

    Added some utility to StringAdditions.
            ToBase64EncodedString(this string plainText)
            ToBase64DecodedString(this string base64EncodedData)
            ToSHA1HashedString(this string input)

--- Red.Utility/StringAdditions.cs
+++ Red.Utility/StringAdditions.cs
@@ -1,29 +1,55 @@
 ﻿using System;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
 namespace Red.Utility
 {
-    static public class StringAdditions
-    {
-        const string digits = "0123456789";
+	static public class StringAdditions
+	{
+		const string digits = "0123456789";
 
-        static public string NumericSuffix(this string aString)
-        {
-            if (aString.Equals(string.Empty)) return aString;
+		static private SHA1CryptoServiceProvider _sha1Provider = new SHA1CryptoServiceProvider();
 
-            int index = aString.Length - 1;
-            while (index > -1 && digits.Contains(aString.Substring(index, 1)))
-            {
-                index--;
-            }
-            return aString.Substring(index + 1);
-        }
+		static public string NumericSuffix(this string aString)
+		{
+			if (aString.Equals(string.Empty)) return aString;
 
-        static public string NextInSequence(this string aString)
-        {
-            string suffix = aString.NumericSuffix();
-            string prefix = aString.Substring(0, aString.Length - suffix.Length);
-            int number = suffix.Length > 0 ? int.Parse(suffix) : 0;
-            number++;
-            return $"{prefix}{number}";
-        }
-    }
+			int index = aString.Length - 1;
+			while (index > -1 && digits.Contains(aString.Substring(index, 1)))
+			{
+				index--;
+			}
+			return aString.Substring(index + 1);
+		}
+
+		static public string NextInSequence(this string aString)
+		{
+			string suffix = aString.NumericSuffix();
+			string prefix = aString.Substring(0, aString.Length - suffix.Length);
+			int number = suffix.Length > 0 ? int.Parse(suffix) : 0;
+			number++;
+			return $"{prefix}{number}";
+		}
+
+		static public string ToBase64EncodedString(this string plainText)
+		{
+			var plainTextBytes = Encoding.UTF8.GetBytes(plainText);
+			return Convert.ToBase64String(plainTextBytes);
+		}
+
+		static public string ToBase64DecodedString(this string base64EncodedData)
+		{
+			var base64EncodedBytes = Convert.FromBase64String(base64EncodedData);
+			return Encoding.UTF8.GetString(base64EncodedBytes);
+		}
+
+		static public string ToSHA1HashedString(this string input)
+		{
+			var hash = _sha1Provider.ComputeHash(Encoding.UTF8.GetBytes(input));
+			return string.Join("", hash.Select(b => b.ToString("x2")).ToArray());
+		}
+
+
+	}
 }
